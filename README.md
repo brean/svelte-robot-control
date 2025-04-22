363b36372315bbb69ae5f2239c14a87962bb9329
Andreas Bresser <self@andreasbresser.de>
2025-04-23 00:29:01 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Svelte Robot Control
+Minimalistic robot control setup as basic setup for your robot control Ground Station.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import {
+  GamepadManager, KeyboardManager, Joystick
+} from "svelte-gamepad-virtual-joystick";
+
+let position: [x: number, y: number] = $state([0, 0]);
+
+// connect to get WebRTC video stream directly
+// we might get this from a websocket-server in the future that also accepts the 
+// virtual joystick values
+</script>
+
+<Joystick
+  style="background-color: rgb(6, 23, 28);"
+  size={120}
+  bind:position={position}
+/>
+
+<!-- GamepadManager and KeyboardManager should be unique in your page. -->
+<GamepadManager />
+<KeyboardManager />
+
